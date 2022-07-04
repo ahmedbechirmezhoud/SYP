5b7ee326dfac147b45b464fb398f1682045c3c67
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-07-04 14:38:49 +0100

    Avoid Keyboard

--- screens/ProfileScreen/ProfileScreen.tsx
+++ screens/ProfileScreen/ProfileScreen.tsx
@@ -1,52 +1,64 @@
-import { Image, Text, TextInput, View } from "react-native";
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import CustomButton from "../../components/CustomButton/CustomButton";
-import { RootTabScreenProps } from '../../types';
+import { RootTabScreenProps } from "../../types";
 import { styles } from "./style";
 
-export default function ProfileScreen({ navigation }: RootTabScreenProps<'Timeline'>) {
-  const changeIEEEIdentifier = () => {
-
-  }
+export default function ProfileScreen({
+  navigation,
+}: RootTabScreenProps<"Timeline">) {
+  const changeIEEEIdentifier = () => {};
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.QRImage} source={require('./../../assets/images/QR.png')} />
-
-      <View style={styles.currentUser}>
-        <Text style={styles.welcome}>
-          WELCOME, BECHIIIIIR
-        </Text>
-
-        <Text style={styles.customizeExp}>
-          We need the IEEE ID to customize your app experience
-        </Text>
-
-        <View style={styles.currentIDContainer}>
-          <Text style={styles.currentIDTitle}>
-            Current ID :
-          </Text>
-          <Text style={styles.currentID}> 5465446464</Text>
-        </View>
-      </View>
-
-      <View style={styles.changeIDSection}>
-        <Text style={styles.title}>
-          NEW IEEE IDENTIFIER
-        </Text>
-
-        <TextInput placeholder="000000000000" style={styles.inputArea} />
-
-        <View style={styles.buttonContainer}>
-          <CustomButton
-            title="CHANGE"
-            onPress={changeIEEEIdentifier}
-            width="60%"
-            bgColor="#F86918CC"
-            color="#FFF4F4"
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <>
+          <Image
+            style={styles.QRImage}
+            source={require("./../../assets/images/QR.png")}
           />
-        </View>
 
-      </View>
-    </View>
+          <View style={styles.currentUser}>
+            <Text style={styles.welcome}>WELCOME, BECHIIIIIR</Text>
+
+            <Text style={styles.customizeExp}>
+              We need the IEEE ID to customize your app experience
+            </Text>
+
+            <View style={styles.currentIDContainer}>
+              <Text style={styles.currentIDTitle}>Current ID :</Text>
+              <Text style={styles.currentID}> 5465446464</Text>
+            </View>
+          </View>
+
+          <View style={styles.changeIDSection}>
+            <Text style={styles.title}>NEW IEEE IDENTIFIER</Text>
+
+            <TextInput placeholder="000000000000" style={styles.inputArea} />
+
+            <View style={styles.buttonContainer}>
+              <CustomButton
+                title="CHANGE"
+                onPress={changeIEEEIdentifier}
+                width="60%"
+                bgColor="#F86918CC"
+                color="#FFF4F4"
+              />
+            </View>
+          </View>
+        </>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
