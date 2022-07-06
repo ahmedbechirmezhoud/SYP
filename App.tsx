7e1df6621e775e96a571e3d5c17feb30b5ecc5c2
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-07-06 13:11:42 +0100

    fetch notofication token

--- App.tsx
+++ App.tsx
@@ -4,8 +4,73 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import Navigation from "./navigation";
 
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { useEffect, useRef, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDX8JG_kPw0oR41PE6Q_J2NW3qJMvuaNcY",
+  authDomain: "r8syp-8062b.firebaseapp.com",
+  projectId: "r8syp-8062b",
+  storageBucket: "r8syp-8062b.appspot.com",
+  messagingSenderId: "640548717558",
+  appId: "1:640548717558:web:bc66e02d1e7731cdb47dd7",
+  measurementId: "G-2RF8G89EW8",
+};
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<any>();
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token || "")
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const firestore = getFirestore();
+
+    const UpdateToken = async (token: string) => {
+      await setDoc(doc(firestore, "users", "97800363"), {
+        NotificationToken: token,
+      });
+    };
+
+    const result = UpdateToken(expoPushToken).catch(console.error);
+  }, [expoPushToken]);
 
   if (!isLoadingComplete) {
     return null;
@@ -17,4 +82,36 @@ export default function App() {
       </SafeAreaProvider>
     );
   }
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
 }
